6bb10273e03a1c2cad78e73ce1ac3a121ad0062d
Ajith ittan <ajithittan@Ajiths-iMac.local>
2019-07-27 20:55:07 -0400

    changes 7/27 -iMac

--- IntelliStockWeb/StockData/models.py
+++ IntelliStockWeb/StockData/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class STK_Code(models.Model):
+    stk_code = models.CharField(max_length=30)
+    stk_Desc = models.CharField(max_length=600)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.stk_code
+
+class STK_Sentiment(models.Model):
+    stk_code = models.ForeignKey(STK_Code, on_delete=models.CASCADE)
+    stk_SentimentInd = models.IntegerField(default=0)
+    stk_DtTmofChg = models.DateTimeField('DtTm of Change')
+
+    def __str__(self):
+        return self.stk_DtTmofChg
