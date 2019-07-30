48e22711c9060c3fb8d16ddd8d391ea104671452
ajithittan <ajithittan@gmail.com>
2019-07-30 19:43:50 -0400

    mac pro

--- IntelliStockWeb/StockData/views.py
+++ IntelliStockWeb/StockData/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import STK_TrigSent
+import datetime
 
 def index(request):
     return render(request, 'StockData/index.html')
@@ -11,6 +13,9 @@ def dispTrigSent(request):
 def setTrigSent(request):
     try:
         retMsg = request.POST['stkId']
+        objtrigsent = STK_TrigSent(stk_code=retMsg,stk_TrigStat=1,stk_TrigDtTm=datetime.datetime.now())
+        objtrigsent.save()
         return render(request, 'StockData/Trigger.html',{'error_message': retMsg})
     except Exception as e:
-        return HttpResponseRedirect(reverse('StockData:index',))
+        print(e)
+        return render(request, 'StockData/Trigger.html',{'error_message': "FUCKED!"})
