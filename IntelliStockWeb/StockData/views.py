59f2300c4baea1edbb3150d43de636322f5e9c00
ajithittan <ajithittan@gmail.com>
2019-07-31 18:28:17 -0400

    Mac Pro.

--- IntelliStockWeb/StockData/views.py
+++ IntelliStockWeb/StockData/views.py
@@ -15,11 +15,20 @@ def dispTrigSent(request):
 def setTrigSent(request):
     try:
         retMsg = request.POST['stkId']
-        stk_code_obj = STK_Code.objects.filter(stk_code=retMsg)
+        stk_code_obj = STK_Code.objects.filter(stk_code=retMsg)[0]
+        print("stk_code_obj",stk_code_obj)
         objtrigsent = STK_TrigSent(stk_code=stk_code_obj,stk_TrigStat=1,stk_TrigDtTm=datetime.datetime.now())
-        print(objtrigsent)
+        #print(objtrigsent)
         objtrigsent.save()
-        return render(request, 'StockData/Trigger.html',{'error_message': retMsg})
+        return render(request, 'StockData/Trigger.html',{'error_message': "Triggered smart analysis for - " + retMsg + " Wait for it!!! "})
+    except Exception as e:
+        print(e)
+        return render(request, 'StockData/Trigger.html',{'error_message': "FUCKED!"})
+
+def getTrigStks (request):
+    try:
+        print("Am I here?")
+        render(request, 'StockData/DispTrigs.html')
     except Exception as e:
         print(e)
         return render(request, 'StockData/Trigger.html',{'error_message': "FUCKED!"})
