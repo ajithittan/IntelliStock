6ab98f3959f88442fda55339227d90e19d2dc35c
Ajith ittan <ajithittan@Ajiths-iMac.local>
2019-08-04 19:51:47 -0400

    iMac - Awesome Drop down.

--- IntelliStockWeb/StockData/views.py
+++ IntelliStockWeb/StockData/views.py
@@ -10,15 +10,19 @@ def index(request):
     return render(request, 'StockData/index.html')
 
 def dispTrigSent(request):
-    return render(request, 'StockData/Trigger.html')
+    stkcds  = STK_Code.objects.filter()
+    return render(request, 'StockData/Trigger.html', {'stkcds':stkcds})
 
 def setTrigSent(request):
     try:
-        retMsg = request.POST['stkId']
-        stk_code_obj = STK_Code.objects.filter(stk_code=retMsg)[0]
-        objtrigsent = STK_TrigSent(stk_code=stk_code_obj,stk_TrigStat=1,stk_TrigDtTm=datetime.datetime.now())
-        objtrigsent.save()
-        return render(request, 'StockData/Trigger.html',{'error_message': "Triggered smart analysis for - " + retMsg + " Wait for it!!! "})
+        if request.method == 'POST':
+            retMsg = request.POST['stkId']
+            stk_code_obj = STK_Code.objects.filter(stk_code=retMsg)[0]
+            objtrigsent = STK_TrigSent(stk_code=stk_code_obj,stk_TrigStat=1,stk_TrigDtTm=datetime.datetime.now())
+            objtrigsent.save()
+            return render(request, 'StockData/Trigger.html',{'error_message': "Triggered smart analysis for - " + retMsg + " Wait for it!!! "})
+        elif request.method == 'GET':
+            return dispTrigSent (request)    
     except Exception as e:
         print(e)
         return render(request, 'StockData/Trigger.html',{'error_message': "FUCKED!"})
@@ -34,10 +38,13 @@ def getTrigStks (request):
 def delTrigStks (request):
     try:
         if request.method == 'POST':
+            print("Am I here?")
             stkTrigId = request.POST['stkTrigId']
             stkCd = request.POST['stkCd']
             objTrigStk = STK_TrigSent.objects.get(id=stkTrigId)
             objTrigStk.delete()
+            return getTrigStks (request)
+        elif request.method == 'GET':
             return getTrigStks (request)
     except Exception as e:
         print (e)
