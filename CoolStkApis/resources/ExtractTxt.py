17a27918d546deef0ffbf6f10a21d600a5f83629
ajithittan <ajithittan@gmail.com>
2019-08-18 21:03:43 -0400

    Trying get params.

--- CoolStkApis/resources/ExtractTxt.py
+++ CoolStkApis/resources/ExtractTxt.py
@@ -5,9 +5,11 @@ import requests
 class ExtractTxt(Resource):
 
     def get(self,inpurl):
+        print("am i here????????")
         print(inpurl)
         req = requests.get(inpurl)
         soup = BeautifulSoup(req.text, 'html.parser')
+        print(soup.text)
         return (soup.text)
 
     def post(self):
