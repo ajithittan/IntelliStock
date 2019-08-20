7f387289fb33022a66a8061d7395b47527410403
ajithittan <ajithittan@gmail.com>
2019-08-19 20:05:18 -0400

    Args still not working.

--- CoolStkApis/app.py
+++ CoolStkApis/app.py
@@ -6,4 +6,4 @@ api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
-api.add_resource(ExtractTxt , '/ExtractTxt/<string:inpurl>')
+api.add_resource(ExtractTxt , '/ExtractTxt')
--- CoolStkApis/resources/ExtractTxt.py
+++ CoolStkApis/resources/ExtractTxt.py
@@ -1,12 +1,19 @@
 from flask_restful import Resource
+from webargs import fields, validate
+from webargs.flaskparser import use_args, use_kwargs, parser, abort
 from bs4 import BeautifulSoup
 import requests
 
+
 class ExtractTxt(Resource):
 
-    def get(self,inpurl):
+    url_args = {"inpURL": fields.Str()}
+
+    @use_args(url_args)
+    def get(self,args):
         print("am i here????????")
-        print(inpurl)
+        inpurl = format(args["inpURL"])
+        print("what is the url", inpurl)
         req = requests.get(inpurl)
         soup = BeautifulSoup(req.text, 'html.parser')
         print(soup.text)
